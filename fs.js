ff44b6f2e6dc83120799e521f118598ea7e05125
alessandra <alessandraamontanher@gmail.com>
2019-11-25 11:14:33 -0300

    develop

--- fs.js
+++ fs.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = {
     saveFile: function(data){
-        fs.writeFile("result.txt", JSON.stringify(data), function(err){
+        fs.writeFile("answer.json", JSON.stringify(data), function(err){
             if(err){
                 return console.log(err);
             }
