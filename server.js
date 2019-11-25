43f9e27ee5a07714699628a4e98b31441e33d4b1
alessandra <alessandraamontanher@gmail.com>
2019-11-25 09:57:30 -0300

    calling codenation API

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const axios = require('axios');
+const https = require('https');
+
+const app = express();
+
+const myToken = 'c8ab0ce60da5344bc3313c5f93099e9b543f682d';
+
+const agent = new https.Agent({
+    rejectUnauthorized: false
+  });
+
+axios.get('https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=' + myToken, { httpsAgent: agent })
+.then(response => {
+    var result = response.data;
+    console.log(result);
+})
+.catch(error => {
+    console.log(error);
+});
+
+app.listen(3333);
