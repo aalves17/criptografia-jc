53d7e28a5ebf862aa90b2415dff6124b6fcd5437
alessandra <alessandraamontanher@gmail.com>
2019-11-26 13:31:02 -0300

    sending challenge

--- api.js
+++ api.js
@@ -1,17 +1,22 @@
+const fs = require('fs');
+const path = require('path')
 const axios = require('axios');
 const https = require('https');
 const util = require('./utils');
+const request = require('request');
 const process = require('./process');
+
+const myToken = 'c8ab0ce60da5344bc3313c5f93099e9b543f682d';
+
+const urlGET = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=' + myToken;
+const urlPOST = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=' + myToken;
+
+const agent = new https.Agent({
+    rejectUnauthorized: false
+});
 
 module.exports = {
     getChallenge: function(){
-        const myToken = 'c8ab0ce60da5344bc3313c5f93099e9b543f682d';
-
-        const urlGET = 'https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=' + myToken;
-
-        const agent = new https.Agent({
-            rejectUnauthorized: false
-          });
 
         axios.get(urlGET, { httpsAgent: agent })
         .then(response => {
@@ -30,6 +35,28 @@ module.exports = {
         });
     },
     sendChallenge: function(){
+        var jsonPath = path.join(__dirname, '..', 'criptografia-jc', 'answer.json');
 
+        const file = fs.createReadStream(jsonPath);
+
+        request({
+            method: 'POST',
+            url: urlPOST,
+            headers: {
+            'Content-Type': 'multipart/form-data',
+            },
+            rejectUnauthorized: false,
+            formData: {
+            answer: file
+            }
+        }, (err, res, body) => {
+            if (err) {
+                console.log(err);
+            }
+
+            console.log(res);
+
+            }
+        );
     }
 }
